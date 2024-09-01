9514cd80192a6f3e3be7deee8dd5e780db5f8d16
SkillzPlayz <130307787+SkillzPlayz@users.noreply.github.com>
2024-09-01 15:44:13 -0400

    Add files via upload

---
+++ main (2).java
@@ -0,0 +1,15 @@
+/* Name: Eclipse Vanson         Title: Unit 0 Project              Date: 8/28/24 */
+// Description: For my portion of this assignment, I wanted to create a little bit of text art so I tried to incorporate my initals 
+// while I was making my little cat creature. The eyes of the cat were made with the first inital of my name "E". The inital of my 
+// middle name "N" were made to be the little stripes of the cat. The ears can little legs of the cat were made of the inital of my last name "V".
+
+public class Main{
+    public static void main(String[] args){
+        System.out.println("      ^-----^ ");  
+        System.out.println("     (E  --  E)");
+        System.out.println("     /NNNNNNNN\\ ");
+        System.out.println("     /         \\");
+        System.out.println("     (NNNNNNNNN)");
+        System.out.println("       V    V   ");
+    }
+}
