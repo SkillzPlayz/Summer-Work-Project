e6188ab38b44c449018c762a87c1b983802f983b
SkillzPlayz <130307787+SkillzPlayz@users.noreply.github.com>
2024-09-01 15:43:29 -0400

    Add files via upload

---
+++ main.java
@@ -0,0 +1,24 @@
+/* Jaskaran, Unit 0 Summer Work Project, 8/29/24 */
+// I don't have a middle name so I used "B" and I thought I should create a shape like a rhombus. 
+public class Main{
+    public static void main(String[] args){
+        System.out.println("        J");
+        System.out.println("       J J"); 
+        System.out.println("      J J J"); 
+        System.out.println("     J J J B"); 
+        System.out.println("    J J J B B");
+        System.out.println("   J J J B B B");
+        System.out.println("  J J J B B B S"); 
+        System.out.println(" J J J B B B S S"); 
+        System.out.println("J J J B B B S S S"); 
+        System.out.println(" J J J B B B S S"); 
+        System.out.println("  J J J B B B S"); 
+        System.out.println("   J J J B B B"); 
+        System.out.println("    J J J B B"); 
+        System.out.println("     J J J B"); 
+        System.out.println("      J J J"); 
+        System.out.println("       J J"); 
+        System.out.println("        J"); 
+        
+   }
+}
